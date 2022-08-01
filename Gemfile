4907dd22b74d6f5ee3b15b8e02095c4c63787d68
Dmytro Yatskiv <dmytro.yatskiv@teladochealth.com>
2022-08-01 18:04:19 +0300

    authentication + decorators

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'rubocop-rails', require: false
 gem 'faker', '~> 2'
 gem 'pagy', '~> 4.10'
+gem 'draper', '~> 4.0'
+gem 'valid_email2', '~> 4.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: :destroy
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by email: params[:email]
+    if user&.authenticate(params[:password])
+      sign_in(user)
+      flash[:success] = "Welcome to the app, #{current_user.name_or_email}!"
+      redirect_to root_path
+    else
+      flash[:warning] = "Incorrect email and/or password!"
+      redirect_to new_sessions_path
+    end
+  end
+
+  def destroy
+
+  end
+end
