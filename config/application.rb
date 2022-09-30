4c0682ffcf2790ec846ff49f3ae75506ba8502b9
Dmytro Yatskiv <dmytro.yatskiv@teladochealth.com>
2022-09-30 10:44:30 +0300

    changed default locale

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module BLOG
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.available_locales = %i[en ua]
-    config.i18n.default_locale = :ua
+    config.i18n.default_locale = :en
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
